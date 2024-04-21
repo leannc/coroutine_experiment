098cab84e6c2c0b88dfe2e5312afbb6573e34d10
kang <hust.wangkang@gmail.com>
2024-04-21 23:41:39 +0800

    第一个小例子

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 //
-// Created by wangkang on 2023/12/3.
+// Created by kang on 2024/4/21.
 //
+#include <coroutine>
 #include <iostream>
+
+
+struct ReturnObject{
+    struct promise_type
+    {
+        std::suspend_never initial_suspend() {return {};}
+        std::suspend_never final_suspend() noexcept {return {};}
+        ReturnObject get_return_object(){return {};}
+        void unhandled_exception() {}
+    };
+};
+
+
+ReturnObject foo()
+{
+    std::cout << "1"<<std::endl;
+    co_await std::suspend_always{};
+    std::cout << "2"<<std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    foo();
 }
